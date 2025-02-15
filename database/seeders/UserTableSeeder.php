255c59b9b8ada6fec0d645c44c621cc1220493d0
hasanarofid <hasanarofid@gmail.com>
2025-02-15 20:55:23 +0700

    first

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -18,14 +18,14 @@ class UserTableSeeder extends Seeder
             [
                 'name' => 'Administrator',
                 'email' => 'admin@gmail.com',
-                'password' => bcrypt('123'),
+                'password' => bcrypt('admin123'),
                 'foto' => '/img/user.jpg',
                 'level' => 1
             ],
             [
                 'name' => 'Kasir 1',
                 'email' => 'kasir1@gmail.com',
-                'password' => bcrypt('123'),
+                'password' => bcrypt('kasir123'),
                 'foto' => '/img/user.jpg',
                 'level' => 2
             ]
